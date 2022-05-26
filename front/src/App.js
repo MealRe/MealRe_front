82670e4b0363cb3725574fd1ed54a841272a3835
woneunji <ji8460@naver.com>
2022-05-26 22:46:15 +0900

    frontttt

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Top from "./component/Top";
-import Content from './component/content';
 
 function App() {
   return (
-    <div>
-      <Top />
-      <Content />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
