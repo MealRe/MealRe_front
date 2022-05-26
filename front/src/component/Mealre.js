dc68193b6bdd0bc010e4d74def9c02dc9f80145d
woneunji <ji8460@naver.com>
2022-05-26 22:51:10 +0900

    ffff

---
+++ front/src/component/Mealre.js
@@ -0,0 +1,8 @@
+
+export default function Mealre() {
+    return (
+    <div>
+        <h1>밀리</h1>
+    </div>
+    );
+}
---
+++ front/src/component/Welcome.js
@@ -0,0 +1,7 @@
+export default function Welcome() {
+    return (
+        <div>
+            <h2>베스트</h2>
+        </div>
+    ); 
+}
